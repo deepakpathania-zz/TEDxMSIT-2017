9ae484f06c410ac697acb941ad1cb54bcc320fad
deepakpathania <deepakpathania789@gmail.com>
2016-12-26 16:42:47 +0530

    configuring for deployment

--- connection.php
+++ connection.php
@@ -1,3 +1,10 @@
 <?php
-$db = mysqli_connect("", "", "", "") or die ("Failed to connect");
+    $hostname = "localhost";
+    $user = "id240176_deepakpathania";
+    $password = "legendary";
+    $database = "id240176_userprofile";
+    $prefix = "";
+    //$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Opps some thing went wrong");
+    //mysql_select_db($mysql_database, $bd) or die("Oops some thing went wrong");
+    $database=mysqli_connect($hostname,$user,$password,$database);
 ?>
